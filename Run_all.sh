222523a38e3ad4bc54ab58a6f4a7a88f81cd1f13
Pedro Alejandro Espín <65787135+alejobeap@users.noreply.github.com>
2025-07-17 11:47:21 +0100

    Create Run_all.sh

---
+++ Run_all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Run shorters connections (2 for each epoch)"
+./create_interefrograms_all_txt.sh
+
+echo "Create wrap and cc for shorters connections (2 for each epoch)"
+./Create_filter_IFS.sh
+
+# Ejecutar el resto tras 2 horas sin bloquear la terminal
+(
+  sleep 7200
+  echo "Estimated the average coherence"
+  python Estimate_Coherence_Average_from_DEM.py
+  ./filtered_average.sh
+  python matriz_coherencia.py
+  ./MesesLargos.sh
+  ./Longs_create_interefrograms_all_txt.sh
+  ./crear_standar_list.sh
+  ./crear_final_list.sh
+) &
+
+echo "La segunda parte del script se ejecutará en 2 horas, mientras puedes seguir usando la terminal."

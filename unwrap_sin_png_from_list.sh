3c46238c579a6d3504ddffc74d76fa7e493d0716
Pedro Alejandro Espín <65787135+alejobeap@users.noreply.github.com>
2025-07-30 10:12:23 +0100

    Update unwrap_sin_png_from_list.sh

--- unwrap_sin_png_from_list.sh
+++ unwrap_sin_png_from_list.sh
@@ -20,9 +20,9 @@ while IFS= read -r file; do
 
   # Si el mes está entre 05 y 09, cambiar a 10 horas para trata de procesar con mala coherencia
   if [[ "$mes" =~ ^0[5-9]$ || "$mes" == "10" ]]; then
-        sbatch --qos=high --output=sbatch_logs/${parent_dir}_$file.out --error=sbatch_logs/${parent_dir}_$file.err --job-name=${parent_dir}_$file -n 8 --time=02:59:00 --mem=65536 -p comet --account=comet_lics --partition=standard --wrap="unwrap_geo.sh `cat sourceframe.txt` $file"
+        sbatch --qos=high --output=sbatch_logs/${parent_dir}_$file.out --error=sbatch_logs/${parent_dir}_$file.err --job-name=${parent_dir}_${file}_${current_dir} -n 8 --time=02:59:00 --mem=65536 -p comet --account=comet_lics --partition=standard --wrap="./unwrap_geo.sh `cat sourceframe.txt` $file"
   else
-        sbatch --qos=high --output=sbatch_logs/${parent_dir}_$file.out --error=sbatch_logs/${parent_dir}_$file.err --job-name=${parent_dir}_$file -n 8 --time=02:59:00 --mem=65536 -p comet --account=comet_lics --partition=standard --wrap="./unwrap_geo.sh `cat sourceframe.txt` $file"
+        sbatch --qos=high --output=sbatch_logs/${parent_dir}_$file.out --error=sbatch_logs/${parent_dir}_$file.err --job-name=${parent_dir}_${file}_${current_dir} -n 8 --time=02:59:00 --mem=65536 -p comet --account=comet_lics --partition=standard --wrap="./unwrap_geo.sh `cat sourceframe.txt` $file"
   fi
   
 done < listaunwpng.txt

fd1a7f032209e85e0a37e6077bfe61087c765a1a
Pedro Alejandro Espín <65787135+alejobeap@users.noreply.github.com>
2025-08-01 11:12:30 +0100

    Update unwrap_run.sh

--- unwrap_run.sh
+++ unwrap_run.sh
@@ -3,4 +3,4 @@ parent_dir=$(basename "$(dirname "$(pwd)")")
 current_dir=$(basename "$(pwd)")
 
 
-bsub2slurm.sh -o sbatch_logs/"UNW_${parent_dir}\_${current_dir}".out -e sbatch_logs/"UNW_${parent_dir}\_${current_dir}".err -J "UNW_${parent_dir}\_${current_dir}" -n 10 -W 47:59 -M 32768 -q comet ./unwrap_sin_png_from_list.sh
+bsub2slurm.sh -o sbatch_logs/"unwrap_${parent_dir}\_${current_dir}".out -e sbatch_logs/"unwrap_${parent_dir}\_${current_dir}".err -J "Unwrap_${parent_dir}\_${current_dir}" -n 10 -W 47:59 -M 32768 -q comet ./unwrap_sin_png_from_list.sh

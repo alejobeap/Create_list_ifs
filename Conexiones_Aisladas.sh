3f8f5276f007dfc59e3db4cdb2c7157a4eee3821
Pedro Alejandro Espín <65787135+alejobeap@users.noreply.github.com>
2025-10-16 13:52:50 +0100

    Update Conexiones_Aisladas.sh

--- Conexiones_Aisladas.sh
+++ Conexiones_Aisladas.sh
@@ -3,7 +3,7 @@
 parent_dir=$(basename "$(dirname "$(pwd)")")
 current_dir=$(basename "$(pwd)")
 
-output="IFSforLiCSBAS_${current_dir}_${parent_dir}_${subsetnumero}.txt"
+
 
 if [ -n "$1" ]; then
     # Caso 1: argumento entregado → usarlo
@@ -28,7 +28,7 @@ else
 fi
 
 
-
+output="IFSforLiCSBAS_${current_dir}_${parent_dir}_${subsetnumero}.txt"
 
 echo "conexiones aisladas"
 python conexiones_aisladas.py
@@ -39,9 +39,17 @@ echo "📄 Número total de combinaciones generadas: $line_count"
 framebatch_gapfill.sh -l -N -I ${PWD}/interferogramasnoaislados.txt 5 200 7 2
 
 
-
-
 cat interferogramasnoaislados.txt >> $output
+
+# Sort the file in-place (overwrite)
+sort -o "$output" "$output"
+
+
+python Check_noloop_GEOC.py
+
+framebatch_gapfill.sh -l -N -I ${PWD}/minimal_loops_GEOC.txt 5 200 7 2
+
+cat  minimal_loops_GEOC.txt >> $output
 
 # Sort the file in-place (overwrite)
 sort -o "$output" "$output"

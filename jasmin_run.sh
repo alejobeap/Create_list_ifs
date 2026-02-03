39d2c90e2735ec30864e2c38be5c88dcfaac4351
Pedro Alejandro Espín <65787135+alejobeap@users.noreply.github.com>
2026-02-03 13:39:54 +0000

    Update jasmin_run.sh

--- jasmin_run.sh
+++ jasmin_run.sh
@@ -1,12 +1,14 @@
 module load licsbas_comet_dev
+parent_dir=$(basename "$(dirname "$(pwd)")")
+current_dir=$(basename "$(pwd)")
 ./batch_LiCSBAS.sh
-LiCSBAS_flt2geotiff.py -i TS_GEOCml2mask/results/vel.filt.mskd -p GEOCml2mask/EQA.dem_par -o 083D_13222_131313.vel_filt.mskd.geo.tif
-#LiCSBAS_flt2geotiff.py -i TS_GEOCml2mask/results/vel.filt -p GEOCml2mask/EQA.dem_par -o 083D_13222_131313.vel_filt.geo.tif
-#LiCSBAS_flt2geotiff.py -i TS_GEOCml2mask/results/vel.mskd -p GEOCml2mask/EQA.dem_par -o 083D_13222_131313.vel.mskd.geo.tif
-#LiCSBAS_flt2geotiff.py -i TS_GEOCml2mask/results/vel -p GEOCml2mask/EQA.dem_par -o 083D_13222_131313.vel.geo.tif
-#LiCSBAS_flt2geotiff.py -i TS_GEOCml2mask/results/vstd -p GEOCml2mask/EQA.dem_par -o 083D_13222_131313.vstd.geo.tif
-#cp TS_GEOCml2mask/network/network13.png 083D_13222_131313'_network.png'
-#cp TS_GEOCml2mask/mask_ts.png 083D_13222_131313'_mask_ts.png'
-#LiCSBAS_out2nc.py -i TS_GEOCml2mask/cum.h5 -o 083D_13222_131313.nc
-LiCSBAS_disp_img.py -i TS_GEOCml2mask/results/vel.filt.mskd -p GEOCml2mask/EQA.dem_par -c SCM.roma_r --cmin -20 --cmax 20 --kmz 083D_13222_131313.vel.mskd.kmz
-#LiCSBAS_disp_img.py -i TS_GEOCml2mask/results/vel.filt.mskd -p GEOCml2mask/EQA.dem_par -c SCM.roma_r --cmin -20 --cmax 20 --title 083D_13222_131313_vel_filt_mskd --png 083D_13222_131313.vel.mskd.png
+LiCSBAS_flt2geotiff.py -i TS_GEOCml2mask/results/vel.filt.mskd -p GEOCml2mask/EQA.dem_par -o ${parent_dir}_${current_dir}.vel_filt.mskd.geo.tif
+#LiCSBAS_flt2geotiff.py -i TS_GEOCml2mask/results/vel.filt -p GEOCml2mask/EQA.dem_par -o ${parent_dir}_${current_dir}.vel_filt.geo.tif
+#LiCSBAS_flt2geotiff.py -i TS_GEOCml2mask/results/vel.mskd -p GEOCml2mask/EQA.dem_par -o ${parent_dir}_${current_dir}.vel.mskd.geo.tif
+#LiCSBAS_flt2geotiff.py -i TS_GEOCml2mask/results/vel -p GEOCml2mask/EQA.dem_par -o ${parent_dir}_${current_dir}.vel.geo.tif
+#LiCSBAS_flt2geotiff.py -i TS_GEOCml2mask/results/vstd -p GEOCml2mask/EQA.dem_par -o ${parent_dir}_${current_dir}.vstd.geo.tif
+#cp TS_GEOCml2mask/network/network13.png ${parent_dir}_${current_dir}'_network.png'
+#cp TS_GEOCml2mask/mask_ts.png ${parent_dir}_${current_dir}'_mask_ts.png'
+#LiCSBAS_out2nc.py -i TS_GEOCml2mask/cum.h5 -o ${parent_dir}_${current_dir}.nc
+LiCSBAS_disp_img.py -i TS_GEOCml2mask/results/vel.filt.mskd -p GEOCml2mask/EQA.dem_par -c SCM.roma_r --cmin -20 --cmax 20 --kmz ${parent_dir}_${current_dir}.vel.mskd.kmz
+#LiCSBAS_disp_img.py -i TS_GEOCml2mask/results/vel.filt.mskd -p GEOCml2mask/EQA.dem_par -c SCM.roma_r --cmin -20 --cmax 20 --title ${parent_dir}_${current_dir}_vel_filt_mskd --png ${parent_dir}_${current_dir}.vel.mskd.png

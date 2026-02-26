ee44fde04e76541355d3afbe43cb78596c97009e
Pedro Alejandro Espín <65787135+alejobeap@users.noreply.github.com>
2026-02-26 15:26:17 +0000

    Create jasmin_run_clip.sh

---
+++ jasmin_run_clip.sh
@@ -0,0 +1,14 @@
+module load licsbas_comet_dev
+parent_dir=$(basename "$(dirname "$(pwd)")")
+current_dir=$(basename "$(pwd)")
+./batch_LiCSBAS_clip.sh
+LiCSBAS_flt2geotiff.py -i TS_GEOCml2maskclip/results/vel.filt.mskd -p GEOCml2maskclip/EQA.dem_par -o ${parent_dir}_${current_dir}_clip.vel_filt.mskd.geo.tif
+#LiCSBAS_flt2geotiff.py -i TS_GEOCml2mask/results/vel.filt -p GEOCml2mask/EQA.dem_par -o ${parent_dir}_${current_dir}.vel_filt.geo.tif
+#LiCSBAS_flt2geotiff.py -i TS_GEOCml2mask/results/vel.mskd -p GEOCml2mask/EQA.dem_par -o ${parent_dir}_${current_dir}.vel.mskd.geo.tif
+#LiCSBAS_flt2geotiff.py -i TS_GEOCml2mask/results/vel -p GEOCml2mask/EQA.dem_par -o ${parent_dir}_${current_dir}.vel.geo.tif
+#LiCSBAS_flt2geotiff.py -i TS_GEOCml2mask/results/vstd -p GEOCml2mask/EQA.dem_par -o ${parent_dir}_${current_dir}.vstd.geo.tif
+#cp TS_GEOCml2mask/network/network13.png ${parent_dir}_${current_dir}'_network.png'
+#cp TS_GEOCml2mask/mask_ts.png ${parent_dir}_${current_dir}'_mask_ts.png'
+#LiCSBAS_out2nc.py -i TS_GEOCml2mask/cum.h5 -o ${parent_dir}_${current_dir}.nc
+LiCSBAS_disp_img.py -i TS_GEOCml2maskclip/results/vel.filt.mskd -p GEOCml2maskclip/EQA.dem_par -c SCM.roma_r --cmin -20 --cmax 20 --kmz ${parent_dir}_${current_dir}_clip.vel.mskd.kmz
+#LiCSBAS_disp_img.py -i TS_GEOCml2mask/results/vel.filt.mskd -p GEOCml2mask/EQA.dem_par -c SCM.roma_r --cmin -20 --cmax 20 --title ${parent_dir}_${current_dir}_vel_filt_mskd --png ${parent_dir}_${current_dir}.vel.mskd.png
